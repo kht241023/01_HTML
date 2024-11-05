84d3fccdfe4f1fedbe7a62163a4339d398089d29
Kyungnan Kim <kht241022@gmail.com>
2024-11-05 17:14:28 +0900

    비교, 삼항연산자

---
+++ 03_JavaScript/js/03_연산자과제1.js
@@ -0,0 +1,19 @@
+/*
+기능 기능명(){
+const n1 = "";
+const n2 = "";
+const result = "";
+
+const comp = (n1 > n2) ? "첫 번째 숫자가 더 큽니다." : "두 번째 숫자가 더 크거나 같습니다.";
+const add = n1 + n2;
+const mul = n1 * n2;
+const sub = n1 - n2;
+
+계산 결과를 result.innerText로 보여주기
+
+결과.innerText = 삼항연산자 +   "\n"  +"더하기 결과: " + add ;
+
+}
+
+
+*/
