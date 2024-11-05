d82821c7efb0f359a519eb90279aa7d014d5f00b
Kyungnan Kim <kht241022@gmail.com>
2024-11-05 17:46:06 +0900

    정답

--- 03_JavaScript/js/03_연산자과제1.js
+++ 03_JavaScript/js/03_연산자과제1.js
@@ -1,18 +1,19 @@
 /*
 기능 기능명(){
-const n1 = "";
-const n2 = "";
-const result = "";
+const n1 = document.getElementById("n1");
+const n2 = document.getElementById("n2");
+const result = document.getElementById("res");
 
 const comp = (n1 > n2) ? "첫 번째 숫자가 더 큽니다." : "두 번째 숫자가 더 크거나 같습니다.";
-const add = n1 + n2;
-const mul = n1 * n2;
-const sub = n1 - n2;
+const add = Number(n1.value) + Number(n2.value);
+const mul = n1.value * n2.value;
+const sub = n1.value - n2.value;
 
 계산 결과를 result.innerText로 보여주기
-
-결과.innerText = 삼항연산자 +   "\n"  +"더하기 결과: " + add ;
-
+result.innerText =               comp + "\n" + 
+                "더하기 결과 : " + add + "\n" + 
+                "곱하기 결과 : " + mul + "\n" + 
+                "빼기 결과 : " + sub;
 }
 
 
