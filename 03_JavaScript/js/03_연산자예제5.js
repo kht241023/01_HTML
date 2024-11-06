269b9cca020680ade49d1952d7954219d35d0442
Kyungnan Kim <kht241022@gmail.com>
2024-11-06 14:16:26 +0900

    삼항연산자 기초 과제

---
+++ 03_JavaScript/js/03_연산자예제5.js
@@ -0,0 +1,21 @@
+const num1 ;//num2
+const num2 ;//num1
+const rst ;//result
+
+
+기능 기능명(){
+    const v1 = 숫자.값;
+    const v2 = 숫자.값;
+    const total = v1 + v2;
+    결과는 이너텍스트 결과확인
+}
+
+
+기능 기능명(){
+    // 홀 짝 유무 확인
+    const sumResult = v1 + v2;
+    const isEven = sumResult % 2           ?  true    :        false ; 
+
+    결과는 이너텍스트 결과확인
+     합   숫자      은(는)    ㅇㅇ 입니다.
+}
