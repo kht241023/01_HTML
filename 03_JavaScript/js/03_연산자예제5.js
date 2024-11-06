309612ea0d8e26bdffa6ec3207b16ac53232d619
Kyungnan Kim <kht241022@gmail.com>
2024-11-06 14:50:41 +0900

    241106 연산자

--- 03_JavaScript/js/03_연산자예제5.js
+++ 03_JavaScript/js/03_연산자예제5.js
@@ -1,21 +1,24 @@
-const num1 ;//num2
-const num2 ;//num1
-const rst ;//result
-
-
-기능 기능명(){
-    const v1 = 숫자.값;
-    const v2 = 숫자.값;
-    const total = v1 + v2;
-    결과는 이너텍스트 결과확인
+const num1 = document.getElementById("num1"); //num2
+const num2 = document.getElementById("num2"); //num2
+const rst = document.getElementById("result"); //result
+//더하기 기능
+function sum() {
+  const v1 = Number(num1.value);
+  const v2 = Number(num2.value);
+  const total = v1 + v2;
+  rst.innerText = "합 : " + total;
 }
 
-
-기능 기능명(){
-    // 홀 짝 유무 확인
-    const sumResult = v1 + v2;
-    const isEven = sumResult % 2           ?  true    :        false ; 
-
-    결과는 이너텍스트 결과확인
-     합   숫자      은(는)    ㅇㅇ 입니다.
+//곱셈 기능
+function mul() {
+  const v1 = Number(num1.value);
+  const v2 = Number(num2.value);
+  const total = v1 * v2;
+  rst.innerText = "곱 : " + total;
+}
+function evenOdd() {
+  // 홀 짝 유무 확인
+  const sumResult = v1 + v2;
+  const isEven = sumResult % 2 == 0 ? "짝수" : "홀수";
+  rst.innerText = "합 " + sumResult + " 은(는) " + isEven + "입니다.";
 }
