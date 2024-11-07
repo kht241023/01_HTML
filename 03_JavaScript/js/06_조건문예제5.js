a8a8f32294007b26b23c5d386f2e22d2b907e018
Kyungnan Kim <kht241022@gmail.com>
2024-11-07 14:34:35 +0900

    241107 조건문

---
+++ 03_JavaScript/js/06_조건문예제5.js
@@ -0,0 +1,24 @@
+/*
+if(       ) - else if(       ) - else
+const month
+const msg
+3 ~ 5월
+봄입니다.
+spring 클래스 추가
+6 ~ 8월
+여름입니다.
+summer 클래스 추가
+9월 ~ 11월
+가을입니다.
+fall 클래스 추가
+12월 1월 2월
+month == 12 || month == 1 || month == 2
+겨울입니다.
+winter 클래스 추가
+나머지
+1에서 12 사이의 숫자를 입력하세요
+텍스트 보이기
+클래스 초기화 
+초기화방법 
+className = ""; 설정
+*/
