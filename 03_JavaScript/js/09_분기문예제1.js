3c8a8fb114c201eb1e97b4f4f6b3b1a703444792
Kyungnan Kim <kht241022@gmail.com>
2024-11-11 16:47:19 +0900

    241111 분기문 () 파라미터 작성시작

--- 03_JavaScript/js/09_분기문예제1.js
+++ 03_JavaScript/js/09_분기문예제1.js
@@ -5,21 +5,28 @@ function selectMenu() {
   while (choice !== null) {
     choice = prompt("메뉴 번호를 입력해주세요. \n 1, 2, 3, 종료 만 입력 가능");
 
-    switch (choice) { 
-      case "1": case "아메리카노":
+    switch (choice) {
+      case "1":
+      case "아메리카노":
         result.innerText = "아메리카노를 선택하셨습니다.";
+        console.log("아메리카노 선택함");
         break;
-      case "2": case "카페라떼":
+      case "2":
+      case "카페라떼":
         result.innerText = "카페라떼를 선택하셨습니다.";
+        console.log("카페라떼 선택함");
         break;
-      case "3": case "프라푸치노":
+      case "3":
+      case "프라푸치노":
         result.innerText = "프라푸치노를 선택하셨습니다.";
+        console.log("프라푸치노 선택함");
         break;
       case "종료":
         result.innerText = "메뉴선택을 종료합니다.";
         return;
       default:
         result.innerText = "잘못된 입력입니다. 다시 선택해주세요.";
+        console.log("잘못 선택함");
         break;
     }
   }
